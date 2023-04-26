51cf85924fb858aa4ba7c5cf9510267b4298c543
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-04-26 16:12:19 +0600

    github and goolge login btn done

--- src/Pages/Shared/RightNav/RightNav.jsx
+++ src/Pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { FaGithub, FaGoogle } from 'react-icons/fa';
 
 const RightNav = () => {
   return (
     <div>
-      <h2>rightNav</h2>
+      <h4>rightNav</h4>
+      <Button className='mb-3' variant="outline-primary"> <FaGoogle />Login With Google</Button>
+      <Button variant="outline-secondary"><FaGithub/> Login With GitHub</Button>
     </div>
   );
 };
