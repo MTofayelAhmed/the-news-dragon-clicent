8956a5bacda334fde051b2363663a166f1191cf1
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-04-26 19:47:23 +0600

    Qzone done

--- src/Pages/Shared/RightNav/RightNav.jsx
+++ src/Pages/Shared/RightNav/RightNav.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { FaGithub, FaGoogle } from 'react-icons/fa';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import {
+  FaGithub,
+  FaGoogle,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import QZone from "../QZone/QZone";
 
 const RightNav = () => {
   return (
     <div>
-      <h4>rightNav</h4>
-      <Button className='mb-3' variant="outline-primary"> <FaGoogle />Login With Google</Button>
-      <Button variant="outline-secondary"><FaGithub/> Login With GitHub</Button>
+      <h4>Login With</h4>
+      <Button className="mb-3" variant="outline-primary">
+        {" "}
+        <FaGoogle />
+        Login With Google
+      </Button>
+      <Button variant="outline-secondary">
+        <FaGithub /> Login With GitHub
+      </Button>
+      <div className="mt-2">
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebook></FaFacebook> FaceBook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter></FaTwitter> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram></FaInstagram> Instagram
+          </ListGroup.Item>
+        </ListGroup>
+        <QZone></QZone>
+      </div>
     </div>
   );
 };
