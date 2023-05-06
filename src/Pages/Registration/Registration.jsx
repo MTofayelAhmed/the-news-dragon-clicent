facc3c4d255b200973ba4c4a7f431fa626d9449c
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-05-06 12:11:21 +0600

    register done

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Registration = () => {
+  const {user,createUser}= useContext(AuthContext)
+const handleRegister = (event)=>{
+event.preventDefault();
+const form = event.target;
+const email = form.email.value;
+const password = form.password.value;
+const name = form.name.value;
+const photo = form.photo.value;
+console.log(email, password, name, photo)
+createUser(email, password)
+.then(result => {
+  const createdUser = result.user;
+  console.log( createdUser)
+  form.reset()
+})
+.catch(error=> {
+  console.log(error.message)
+})
+}
+
+
   return (
-    <div>
-      <h2>this is register</h2>
-    </div>
+    <Container className="w-50 ">
+      <Form onSubmit={handleRegister}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Your name"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Photo Url</Form.Label>
+          <Form.Control
+            type="text"
+            name="photo"
+            placeholder="your Photo"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check name='accept' type="checkbox" label="Accept terms & conditions" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+        <br />
+
+        <Form.Text className="text-danger"></Form.Text>
+        <Form.Text className="text-secondary">
+          Already have an account <Link to="/login">Login</Link>
+        </Form.Text>
+        <Form.Text className="text-success"></Form.Text>
+      </Form>
+    </Container>
   );
 };
 
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,8 +1,30 @@
 import React, { createContext } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from '../firebase/firebase.config';
+
+
+
 export const AuthContext = createContext(null)
+const auth = getAuth(app);
+
+
 const AuthProvider = ({children}) => {
 const user =null
-  const authInfo = {user}
+
+const createUser = (email, password)=>{
+  return createUserWithEmailAndPassword (auth, email, password)
+}
+
+
+
+
+
+  const authInfo = {user, createUser}
+
+
+
+
+
   return (
     <AuthContext.Provider value={authInfo}>
       {children}
